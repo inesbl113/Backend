9a72a74e412ce2617449bf8662c0709bc0485bed
inesbl113 <inesbl113@gmail.com>
2024-06-25 16:21:57 +0200

    addded swagger to timeline

--- Program.cs
+++ Program.cs
@@ -1,13 +1,15 @@
 using CLONETRELLOBACK.Data;
 using CLONETRELLOBACK.Models;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews()
+builder
+    .Services.AddControllersWithViews()
     .AddNewtonsoftJson(options =>
     {
         options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
@@ -15,7 +17,8 @@ builder.Services.AddControllersWithViews()
 
 // Configure the DbContext with a connection string from configuration
 builder.Services.AddDbContext<TaskContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 // Add the TaskDao, ProjectDao, ListDao, and CommentDao to the DI container
 builder.Services.AddScoped<TasksDao>();
@@ -25,13 +28,15 @@ builder.Services.AddScoped<CommentDao>();
 
 builder.Services.AddCors(options =>
 {
-    options.AddDefaultPolicy(
-        builder =>
-        {
-            builder.WithOrigins("*")
-                .AllowAnyHeader()
-                .AllowAnyMethod();
-        });
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins("*").AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "TodoApi", Version = "v1" });
 });
 
 var app = builder.Build();
@@ -43,6 +48,14 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
+    app.UseDeveloperExceptionPage();
+}
+
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoApi v1"));
 }
 
 app.UseHttpsRedirection();
