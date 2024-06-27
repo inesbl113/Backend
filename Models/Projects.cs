bc331633b097e3acec1b5fdc3ecb95af41a80146
inesbl113 <inesbl113@gmail.com>
2024-06-27 13:25:44 +0200

    New list array in project creation

--- Models/Projects.cs
+++ Models/Projects.cs
@@ -2,22 +2,18 @@ namespace CLONETRELLOBACK.models
 {
     public class Projects
     {
-
         public int Id { get; set; } // Identifiant unique du projet
         public string Title { get; set; } // Nom du projet
         public string Description { get; set; } // Description du projet
-        public DateTime CreatedAt { get; set; }  // Date de création du projet
+        public DateTime CreatedAt { get; set; } // Date de création du projet
 
-         // Navigation properties
-         public ICollection<Lists> Lists { get; set; } // Collection de listes associées au projet
+        // Navigation properties
+        public ICollection<Lists> Lists { get; set; } // Collection de listes associées au projet
 
-         public Projects()
+        public Projects()
         {
             CreatedAt = DateTime.Now;
+            Lists = new List<Lists>();
         }
-    
     }
-
 }
-
-
--- obj/Debug/net8.0/back.AssemblyInfo.cs
+++ obj/Debug/net8.0/back.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe5ec0f2085911a222ad429a5f1a3cfc01468b5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86e289fbb6a6cdc22be73d8bc4c20cd1912bd5b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
